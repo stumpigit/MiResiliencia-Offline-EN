45b2855866eec34d7fdd0aacaafb3b8d7c14620a
Christoph Suter <christoph@suter-burri.ch>
2024-09-04 15:53:49 +0200

    Adapted SplashScreen

--- GUI/Properties/AssemblyInfo.cs
+++ GUI/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MiResiliencia Desktop")]
-[assembly: AssemblyDescription("MiResiliencia Desktop")]
+[assembly: AssemblyTitle("MiResiliencia Desktop - Mining")]
+[assembly: AssemblyDescription("MiResiliencia Desktop - Mining")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("GEOTEST AG")]
+[assembly: AssemblyCompany("GEOTEST AG / Christoph Suter")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
 [assembly: AssemblyInformationalVersion("1.2.0")]
